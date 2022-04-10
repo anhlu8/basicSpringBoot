f630a8050fa61e87f499b8c6d92cf02493477252
LU, ANH <ANH_LU@comcast.com>
2022-04-10 13:09:57 -0400

    implement jpa

--- src/main/java/com/alu/employeeapplication/service/EmployeeService.java
+++ src/main/java/com/alu/employeeapplication/service/EmployeeService.java
@@ -1,6 +1,8 @@
 package com.alu.employeeapplication.service;
 
 import com.alu.employeeapplication.entity.Employee;
+import com.alu.employeeapplication.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -15,39 +17,46 @@ public class EmployeeService {
             new Employee(3, "Owen", "Paoli")
     ));
 
-    public List<Employee> getAllEmployees(){
-        return employeeList;
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        //return employeeList;
+        return employeeRepository.findAll();
     }
 
     public Employee getAnEmployee(int id){
-        return employeeList.stream().filter(employee -> employee.getEmployeeId() == id).findFirst().get();
+//        return employeeList.stream().filter(employee -> employee.getEmployeeId() == id).findFirst().get();
+        return employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Not found"));
     }
 
     public void createEmployee(Employee employee){
-        employeeList.add(employee);
+        //employeeList.add(employee);
+        employeeRepository.save(employee);
     }
 
     public void updateEmployee(Employee employee){
-        List<Employee> newEmployeeList = new ArrayList<>();
-        for(Employee e: employeeList){
-            if(e.getEmployeeId() == employee.getEmployeeId()){
-                e.setEmployeeName(employee.getEmployeeName());
-                e.setEmployeeCity(employee.getEmployeeCity());
-            }
-            newEmployeeList.add(e);
-        }
-        this.employeeList = newEmployeeList;
+//        List<Employee> newEmployeeList = new ArrayList<>();
+//        for(Employee e: employeeList){
+//            if(e.getEmployeeId() == employee.getEmployeeId()){
+//                e.setEmployeeName(employee.getEmployeeName());
+//                e.setEmployeeCity(employee.getEmployeeCity());
+//            }
+//            newEmployeeList.add(e);
+//        }
+//        this.employeeList = newEmployeeList;
+        employeeRepository.save(employee);
     }
 
     public void deleteEmployee(int id){
-        List<Employee> tempEmployees = new ArrayList<>();
-        for(Employee emp: employeeList){
-            if(emp.getEmployeeId() == id){
-                continue;
-            }
-            tempEmployees.add(emp);
-        }
-        this.employeeList = tempEmployees;
-
+//        List<Employee> tempEmployees = new ArrayList<>();
+//        for(Employee emp: employeeList){
+//            if(emp.getEmployeeId() == id){
+//                continue;
+//            }
+//            tempEmployees.add(emp);
+//        }
+//        this.employeeList = tempEmployees;
+            employeeRepository.delete(employeeRepository.getById(id));
     }
 }
